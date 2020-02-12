a96b294a16f6ec22b62dc975624ad0f8671c6dc9
mikepetrillo20 <mikepetrillo20@gmail.com>
2020-02-12 17:40:51 -0500

    adding array parser

--- csv_parser.py
+++ csv_parser.py
@@ -1,12 +1,19 @@
 import csv
 
-class ListParser:
+class ParserUsingArray:
 
     def __init__(self):
         self.rows = []
         self.titles = []
+    
+    # prints the title row then each following row according to a user input number
+    def read(self, number_of_rows=9999):
+        print(self.titles)
+        for row in self.rows[:number_of_rows]:
+            print(row)
 
-    def parse_to_list(self, csv_file):
+    # parses the csv file feeding the data received into two arrays
+    def parse_to_array(self, csv_file):
         # reading the csv file
         with open(csv_file, 'r') as csvfile:
             # creating the csv reader object
@@ -18,8 +25,9 @@ class ListParser:
             # grabbing each data row one by one
             for row in csvreader:
                 self.rows.append(row)
+    
+# TESTING for ParserUsingArray
+test = ParserUsingArray()
+test.parse_to_array('small_data.csv')
+test.read(10)
 
-
-test = ListParser()
-test.parse_to_list('small_data.csv')
-print(test.rows)
